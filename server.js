58927fe535ab78887ec9ee01842a6327c19790d8
Christian Tellnes <christian@tellnes.no>
2017-04-23 22:17:00 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const express = require('express')
+const jsonParser = require('body-parser').json
+
+const app = express()
+
+let lastGong
+
+app.get('/gong/gong', (req, res, next) => {
+  res.json(lastGong || null)
+})
+
+app.post('/gong/gong'
+  , jsonParser()
+  , (req, res, next) => {
+      const body = req.body
+      const loc = body.loc || {}
+      lastGong =
+        { name: String(body.name || '')
+        , location:
+          { latitude: parseFloat(loc.latitude)
+          , longitude: parseFloat(loc.longitude)
+          }
+        }
+      res.json({ ok: true })
+  })
+
+app.listen(2355)
